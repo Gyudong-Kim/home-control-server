700fda13cc9fae3c093b9ffb67d9620c976f1fa0
Gyudong Kim <dong3955@gmail.com>
2026-05-25 03:08:36 +0900

    [Chore] 서비스 파일 경로 수정

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-sudo cp home-control-server.service /etc/systemd/system/
+sudo cp service/home-control-server.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo systemctl enable --now home-control-server
